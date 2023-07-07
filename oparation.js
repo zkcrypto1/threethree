db7d88d80e46734a3a6600d88ec9f9275013344c
zk crypto <136746130+zkcrypto1@users.noreply.github.com>
2023-07-08 00:21:20 +0600

    Add files via upload

---
+++ oparation.js
@@ -0,0 +1,3 @@
+var ricePrice = 58;
+var rutiPrice = 10;
+console.log(rutiPrice);
---
+++ variables.js
@@ -0,0 +1,6 @@
+var price = 10;
+var name = "kuddus mia";
+var ismarkemtpy = false;
+var didthework = true;
+
+// variables name///////////////
